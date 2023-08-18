2a63a6903d4b1317db6f6c36b18a13310bb5543f
DIALQGUE <fang.wanna.wannasin@gmail.com>
2023-08-18 16:36:10 +0700

    chore(project): :boom: change from `JavaScript` to `TypeScript`

--- server/server.ts
+++ server/server.ts
@@ -2,4 +2,9 @@ import express from 'express';
 
 const app = express();
 
+app.get('/products', (req, res) => {
+  const products = {};
+  res.json(products);
+});
+
 app.listen(5000, () => console.log('Server running on port 5000 🚀'));
